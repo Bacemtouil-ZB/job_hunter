98d3589df550886de37dccb6c6f42c25fb5ef488
yahya zarred <yahyazarred1234@gmail.com>
2025-11-23 02:58:01 +0100

    worked on backend

---
+++ client/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import ContextProvider from "@/providers/contextProvider";
+import { Roboto } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>
+        <Toaster position="top-center" />
+        <ContextProvider>{children}</ContextProvider>
+      </body>
+    </html>
+  );
+}
